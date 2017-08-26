26b47e0aa364cc3c5597b26f55d25f0470a4e6b9
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2017-08-27 00:41:56 +0800

    Removing ViewModels and Exercise 2 Completion

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,37 +5,25 @@ using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() {Name = "Shrek!"};
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
 
-        // Activate the Attribute Routing in RouteConfig and define the routing with the constraint (min, max, minlength, maxlength, int, float, guid, range) here.
-        // Google ASP.NET MVC Attribute Route Constraint for details.
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        public IEnumerable<Movie> GetMovies()
         {
-            return Content(year + "/" + month);
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
         }
     }
 }
