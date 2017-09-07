a913894169755684bfd2e7b2ee555cd8522fb790
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2017-09-07 21:22:40 +0800

    Creating DTO (Data Transfer Object) for the customer API for more stable API

---
+++ Vidly/Dto/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dto
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        // Display attribute is not required as it is for the view. This is for the API (data display only)
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        // Remove MembershipType as it is a model class, and it create dependency to the model class
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
