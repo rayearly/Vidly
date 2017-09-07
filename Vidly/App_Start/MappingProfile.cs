0698b4a31cceb65de8dc7d7340a78b27066804f7
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2017-09-07 21:36:28 +0800

    Creating AutoMapper MappingProfile in App_Start and initialize it in Global.asax

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dto;
+using Vidly.Models;
+
+// class loaded when application started by adding a line of initialization in Global.asax
+namespace Vidly.App_Start
+{
+    // Derive class from Profile in AutoMapper namespace
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Create a mapping configuration between two types - Do it vice versa
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
