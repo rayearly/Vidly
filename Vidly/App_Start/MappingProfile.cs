56be8aa10e063bebfaf66883da903f62406d3215
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2017-09-08 10:44:20 +0800

    Exercise 6 Completion - resolving ID mapping issues during PUT request

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -15,12 +15,13 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             // Create a mapping configuration between two types - Do it vice versa
-            Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            // .ForMember(m => m.Id, opt => opt.Ignore() will ignore ID mapping during update since updating ID will throw exception
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(m => m.Id, opt => opt.Ignore());
 
             // Create a mapping configuration for MoviesDto to Movies object model
-            Mapper.CreateMap<Movie, MovieDto>();
-            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
 
         }
     }
